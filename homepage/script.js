435ced632966f4e0d3dea7606151724b5ea9c5ed
Vineethmonis <vineeth.monis098@gmail.com>
2026-02-20 20:24:22 +0530

    responsiveness finally done

---
+++ homepage/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+  const navItems = document.querySelectorAll(".nav-links a");
+
+  // Toggle menu
+  hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+  });
+
+  // Close when link clicked
+  navItems.forEach(function(link) {
+    link.addEventListener("click", function () {
+      hamburger.classList.remove("active");
+      navLinks.classList.remove("active");
+    });
+  });
+
+});
